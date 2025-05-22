c8dce58434b44db07b84bb5318ec5d1c2689de78
Nandhitha <nandhithap.22cse@kongu.edu>
2025-05-22 15:12:52 +0530

    my commit6

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div><h1>Hello! Welcome!! This is Home Page!!!! (web)</h1></div>
+        <div><h1>Welcome!! This is Home Page!!!! (web)</h1></div>
     )
 }
